24d3da308d38b77493455feb812975447becae9d
jacobnathanson <jacobnath93@gmail.com>
2022-04-15 12:35:15 -0400

    union

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,33 @@ import React from 'react';
 import './App.css';
 
 let name: string;
-let age: number;
+let age: number | string; // union type one or more value types
 let isStudent: boolean;
 let hobbies: string[];
+let role: [number, string];
 
 // tuple
 // array of strict multiple types
 // tuple the order and length are guaranteed.
-let role: [number, string];
-
-// role = [5,'r']
-
-// create person object from custom type
-type Person = {
-  name: string,
-  age?: number
-}
-// needs both properties
-// 'age is optional, no error 
-let person: Person = {
-  name: 'bob',
-}
-
-// array of person objects
-let lotsOfPeoople:Person[];
 
 
+// // role = [5,'r']
+
+// // create person object from custom type
+// type Person = {
+//   name: string,
+//   age?: number
+// }
+// // needs both properties
+// // 'age is optional, no error 
+// let person: Person = {
+//   name: 'bob',
+// }
+
+// // array of person objects
+// let lotsOfPeoople:Person[];
+
+age = true
 function App() {
   return (
     <div className="App">
