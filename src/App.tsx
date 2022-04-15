809b9cfac3cfca7df2fa24d3c665905a7e31c7b5
jacobnathanson <jacobnath93@gmail.com>
2022-04-15 12:30:17 -0400

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
 
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+
+// tuple
+// array of strict multiple types
+// tuple the order and length are guaranteed.
+let role: [number, string];
+
+// role = [5,'r']
+
+// create person object from custom type
+type Person = {
+  name: string,
+  age: number
+}
+// needs both properties
+let person: Person = {
+  name: 'bob',
+  age: 500
+}
+
+
 function App() {
   return (
     <div className="App">
-  Hello Bob
+      Hello Bob
     </div>
   );
 }
