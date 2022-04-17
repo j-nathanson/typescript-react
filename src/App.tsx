207876a57f44e26b856787f235f3b2eebcd87e39
jacobnathanson <jacobnath93@gmail.com>
2022-04-17 17:14:58 -0400

    basic obj array type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 
 function App() {
@@ -9,10 +10,26 @@ function App() {
     first: 'Bob',
     last: 'Bobson'
   }
+
+  const nameList = [
+    {
+      first: 'Bob',
+      last: 'Bobson'
+    },
+    {
+      first: 'Rob',
+      last: 'Robson'
+    },
+    {
+      first: 'Sob',
+      last: 'Sobson'
+    }
+  ]
   return (
     <div className="App">
       <Greet name='string' messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
