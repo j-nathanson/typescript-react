43e8ac50c8422b98b2286d659f758973570ac230
jacobnathanson <jacobnath93@gmail.com>
2022-04-17 17:06:19 -0400

    typing object prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 
 function App() {
+  const personName = {
+    first: 'Bob',
+    last: 'Bobson'
+  }
   return (
     <div className="App">
-      <Greet name='string'/>
+      <Greet name='string' messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 //use types for application, interface for libraries, but are very similar
-type GreetProps = { name: string }
+type GreetProps = {
+    name: string,
+    messageCount: number,
+    isLoggedIn: boolean
+}
 
 // need to define what type structure the props are
-export const Greet = (props:GreetProps) => {
+export const Greet = (props: GreetProps) => {
     return (
         <div>
-            <h2>hello {props.name}</h2>
+            <h2>
+                {
+                    props.isLoggedIn
+                        ? `hello ${props.name} you have ${props.messageCount} unread messages`
+                        : `Welcome Guest`
+                }
+            </h2>
         </div>
     )
 }
